5bc4c2d24193760650199b25ef44f369da5989e5
ervanhohoho <ervandharmawanhalim@gmail.com>
2017-11-04 17:21:40 +0700

    minor change

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -10,6 +10,7 @@
 {{csrf_field()}}
 	<center>
 	<table border = 0 style = "position: absolute;left: 0;top: 35%; width: 100%;text-align: center;font-size: 18px; z-index: 3;">
+	<tr><td><h1>LOGIN</h1></td></tr>
 	<tr><td><input type="text" name="email" placeholder = "email""><br></td></tr>
 	<tr><td><input type = "password" name = "password" placeholder="password"></td></tr>
 	<tr><td><input type="submit"></td></tr>

374ec4190e8b599fb94eb52f17d142271d406bef
ervanhohoho <ervandharmawanhalim@gmail.com>
2017-11-04 22:06:26 +0700

    view catalog v2

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,16 +1,86 @@
-<!DOCTYPE html>
-<html>
+<html lang="en">
+@extends('layouts.master')
 <head>
-	<title>Catalog</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>4 Col Portfolio - Start Bootstrap Template</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/4-col-portfolio.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <style type="text/css">
+	.container-data
+	{
+		position: relative;
+		top: 20px;
+		padding: 10px 70px;
+	}
+    </style>
 </head>
+
 <body>
-	@if(isset($data))
-		@foreach($data as $d)
-			<img src="{{$d->image}}" width="100">
-			<br>
-			<hr>
-		@endforeach
-	@endif
-	{{$data->links()}}
-</body>
-</html>
+
+    @yield('navbar')
+
+    <!-- Page Content -->
+    <div class="container-data">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Catalogue
+                </h1>
+            </div>
+        </div>
+        <!-- /.row -->
+
+        <!-- Projects Row -->
+        @if(isset($data))
+			@foreach($data as $d)
+	        <div class="row">
+	            <div class="col-md-3 portfolio-item">
+	               
+	                <a href="#">
+	                	<div style =  " display: table-cell; background-image: url({{$d->image}}); width: 100px; height: 100px; background-size:contain; background-repeat:   no-repeat; text-align: top;">
+	                    </div>
+	                     <p style = "display: table-cell; text-align: top; height: 100px"> {{$d->name}}</p>
+	                </a>
+	                	
+	            </div>
+	        </div>
+	        <!-- /.row -->
+	        @endforeach
+		@endif
+        <!-- Pagination -->
+        <div class="row text-center">
+            <div class="col-lg-12">
+                <ul class="pagination">
+                    {{$data->links()}}
+                </ul>
+            </div>
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body></html>
